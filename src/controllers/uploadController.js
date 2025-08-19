782cdd3e0f24bf1b04bac51b8f584ff34df073dd
Harshitr10 <1012harshit@gmail.com>
2025-08-19 09:10:48 +0530

    fixed route

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -12,27 +12,23 @@ export const uploadImages = async (req, res) => {
     const db = getDB();
     const pendingCollection = db.collection("pending");
 
-    // Base directory for storage
-    const baseDir = "/home/harshit/Desktop/quest_test";
+    // const baseDir = "/home/harshit/Desktop/quest_test";
+    const baseDir = "Users/swapac/Downloads/image";
     const targetDir = path.join(baseDir, `pending_${envId}`);
     const imagesDir = path.join(targetDir, "images");
 
-    // If folder already exists → delete it
     if (fs.existsSync(targetDir)) {
       fs.rmSync(targetDir, { recursive: true, force: true });
     }
 
-    // Recreate folders
     fs.mkdirSync(imagesDir, { recursive: true });
 
-    // Save uploaded files into "images" folder
     req.files.forEach((file, index) => {
       const newFilename = `${Date.now()}_${index}_${file.originalname}`;
       const newPath = path.join(imagesDir, newFilename);
       fs.renameSync(file.path, newPath);
     });
 
-    // Prepare single DB record (no per-file data)
     const record = {
       envId,
       folderPath: imagesDir,
@@ -40,19 +36,18 @@ export const uploadImages = async (req, res) => {
       createdAt: new Date(),
     };
 
-    // Insert single record (replace old if envId exists)
     await pendingCollection.updateOne(
       { envId },
       { $set: record },
-      { upsert: true } // creates if not exists
+      { upsert: true } 
     );
 
     res.json({
-      message: "✅ Images uploaded & saved to DB",
+      message: "Images uploaded & saved to DB",
       data: record,
     });
   } catch (error) {
-    console.error("❌ Upload error:", error);
+    console.error("Upload error:", error);
     res.status(500).json({ error: "Something went wrong" });
   }
 };
